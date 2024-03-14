c68b8de4f0d908660b2777a99620c52db7540f5b
Dillibabu <dilli3399@gmail.com>
2024-03-14 18:03:31 +0530

    predicate comment added

--- functionalProgramming/src/Topic_Predicate.java
+++ functionalProgramming/src/Topic_Predicate.java
@@ -3,6 +3,8 @@ import java.util.function.Predicate;
 public class Topic_Predicate {
     public static void main(String[] args) {
         System.out.println(isPhoneNumberIsValid.test("0334567891442"));
+        // isPhoneNumberIsValid.and(otherPredicate).test("input");
+        // isPhoneNumberIsValid.or(otherPredicate).test("input");
     }
 
 
